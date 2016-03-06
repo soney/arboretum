c1df7f3ccc3797589f842316c951b6c63af664f0
Steve Oney <swloney@gmail.com>
2016-03-05 19:39:38 -0500

    syncing

--- client/client_pages/js/menu.js
+++ client/client_pages/js/menu.js
@@ -3,6 +3,51 @@ $.widget('arboretum.menu', {
 		state: false,
 	},
 	_create: function() {
+		this.menu_element = $('<img />', {
+			src: 'images/icon.png'
+		}).css({
+			height: '30px',
+			right: '5px',
+			top: '5px',
+			position: 'fixed',
+			cursor: 'pointer',
+			opacity: 0.3
+		}).appendTo(this.element)
+		.on('mouseover', $.proxy(this._onMenuMouseover, this))
+		.on('mouseout', $.proxy(this._onMenuMouseout, this))
+		.on('click', $.proxy(this._onMenuClick, this));
+
+		this._expanded = false;
+	},
+	_updateOpacity: function() {
+		this.menu_element.css({
+			opacity: this._isExpanded() ? 1 : (this._isHovering() ? 0.5 : 0.3)
+		});
+	},
+	_onMenuMouseover: function() {
+		this._hovering = true;
+		this._updateOpacity();
+	},
+	_onMenuMouseout: function() {
+		this._hovering = false;
+		this._updateOpacity();
+	},
+	_onMenuClick: function() {
+		if(this._isExpanded()) {
+			this._collapse();
+		} else {
+			this._expand();
+		}
+		this._updateOpacity();
+	},
+	_isExpanded: function() {
+		return this._expanded;
+	},
+	_expand: function() {
+		this._expanded = true;
+	},
+	_collapse: function() {
+		this._expanded = false;
 	},
 	_destroy: function() {
 	}
