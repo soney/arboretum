4fae76c608c194ba7c8505f72942c99d2139b7bd
Steve Oney <swloney@gmail.com>
2016-09-26 19:37:41 -0400

    added maker

--- package_browser.js
+++
@@ -1,9 +0,0 @@
-var packager = require('electron-packager')
-packager({
-    dir: '.',
-    platform: 'darwin',
-    arch: 'x64'
-}, function (err, appPaths) {
-    console.error(err);
-    console.log(appPaths)
-})
